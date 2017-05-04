2f0e9d69535f9fc95ad477d2d6b65edf09addbf0
Csaba Kothencz <kothenczcsaba95@gmail.com>
2017-05-04 19:59:02 +0200

    DatasetGraphViewTest test class

---
+++ simple/src/test/java/org/apache/commons/rdf/simple/DatasetGraphViewTest.java
@@ -0,0 +1,13 @@
+package org.apache.commons.rdf.simple;
+
+import org.apache.commons.rdf.simple.experimental.DummyTestDataset;
+
+/**
+ * Created by Csaba Kothencz on 2017. 05. 04.
+ */
+public class DatasetGraphViewTest {
+
+    DatasetGraphView datasetGraphView = new DatasetGraphView(new DummyTestDataset());
+
+
+}
