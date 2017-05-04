e2d523a5a8aa1c313f5afe469c4a7a13b983a32a
Csaba Kothencz <kothenczcsaba95@gmail.com>
2017-05-04 20:07:02 +0200

    closeTest

--- simple/src/test/java/org/apache/commons/rdf/simple/DatasetGraphViewTest.java
+++ simple/src/test/java/org/apache/commons/rdf/simple/DatasetGraphViewTest.java
@@ -1,6 +1,7 @@
 package org.apache.commons.rdf.simple;
 
 import org.apache.commons.rdf.simple.experimental.DummyTestDataset;
+import org.junit.Test;
 
 /**
  * Created by Csaba Kothencz on 2017. 05. 04.
@@ -9,5 +10,13 @@ public class DatasetGraphViewTest {
 
     DatasetGraphView datasetGraphView = new DatasetGraphView(new DummyTestDataset());
 
+    @Test
+    public void closeTest(){
 
+        try {
+            datasetGraphView.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
